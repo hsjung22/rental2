d13ec2fb12a0e439e71dbca3d896cf094533d794
hsjung22 <hsjung22@gmail.com>
2017-01-14 18:30:24 -0800

    allow additional parameters

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -69,6 +69,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:name, :phone_number)
+      params.require(:client).permit(:name, :phone_number, :introduction, :summary)
     end
 end
