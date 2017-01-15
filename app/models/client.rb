998de5e7fc42a991e11906e7ddd875c25eaf1db1
hsjung22 <hsjung22@gmail.com>
2017-01-14 18:09:37 -0800

    update clients with simple-form

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ActiveRecord::Base
 	has_many :reservations
 	has_many :cars, through: :reservations
+
+  validates :name, presence: true
+  validates :phone_number, presence: true, uniqueness: true
 end
